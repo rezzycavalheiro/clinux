6a47dff1ea3b6218b75fbd6c35506d040216ee2f
Renata Cavalheiro da Silva <recavalheiro89@gmail.com>
2019-03-27 09:13:54 -0300

    Update sumofdigits.c

--- sumofdigits.c
+++ sumofdigits.c
@@ -1,4 +1,6 @@
-/* Please write a C-program that uses a recursive function called "sumOfDigits()" to compute the sum of the digits of a number. To do so, your main function should first read an integer number as input and then call sumOfDigits(), which should in turn call itself until there are no digits left to sum, at which point the final sum should display to the user.
+/* Please write a C-program that uses a recursive function called "sumOfDigits()" to compute the sum of the digits of a number. 
+To do so, your main function should first read an integer number as input and then call sumOfDigits(), which should in turn 
+call itself until there are no digits left to sum, at which point the final sum should display to the user.
 Here is the main idea of how the recursion in sumOfDigits() should work:
 sumOfDigits(6452) = 2 + sumOfDigits(645)
 sumOfDigits(645) = 5 + sumOfDigits(64)
